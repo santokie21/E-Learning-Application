ccbbc767b39c36d9ca1faa0f222b816c8cc62d61
Santhosh S <officialuse3012@gmail.com>
2024-03-11 10:45:58 +0530

    Initail commit

---
+++ src/test/java/io/noobi/elearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package io.noobi.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+  @Test
+  void contextLoads(){
+  }
+
+}
